d42333808c47247e86c9cbc580ae8603ac172655
BuildTools <unconfigured@null.spigotmc.org>
2021-01-08 16:45:12 -0700

    even newer mute command

--- commands/mute.js
+++ commands/mute.js
@@ -12,7 +12,7 @@ module.exports = {
                 let memberTarget = message.guild.members.cache.get(target.id);
      
                 if (!args[1]) {
-                    memberTarget.roles.add(muteRole.id);
+                    memberTarget.roles.add(muteRole);
                     message.channel.send(`<@${memberTarget.user.id}> has been muted!`);
                     return
                 }
@@ -20,7 +20,7 @@ module.exports = {
                 message.channel.send(`<@${memberTarget.user.id}> has been muted for ${ms(ms(args[1]))}`);
      
                 setTimeout(function () {
-                    memberTarget.roles.remove(muteRole.id);
+                    memberTarget.roles.remove(muteRole);
                 }, ms(args[1]));
             } else {
                 message.channel.send('Cant find that member!');
