1268d523305960ed35dc4f9c337aec6265a21e9f
BuildTools <unconfigured@null.spigotmc.org>
2021-02-26 14:37:36 -0700

    8ball test

--- commands/fun/8ball.js
+++ commands/fun/8ball.js
@@ -1,24 +1,22 @@
-const Discord = require("discord.js");
+const { MessageEmbed } = require('discord.js')
 
 module.exports = {
-    commands: '8ball',
+    commands: ['8ball', 'eightball'],
     minArgs: 1,
     maxArgs: 1,
-    expectedArgs: '<question>',
+    expectedArgs: "<question>",
     callback: async (message, arguments, text) => {
+        let replies = ["Yes.", "No.", "I don't know.", "Ask again later.", "100% YES!!!!", "Definitiely Not.", "That's a hard no."]
 
-        //.8ball <question ghjgajkdj>
-        let replies = ["Yes.", "No.", "I don't know.", "Ask again later.", "100% YES!!!!", "Definitiely Not.", "That's a hard no."];
+        let result = Math.floor((Math.random() * replies.length))
+        let question = arguments.slice(0).join(" ")
 
-        let result = Math.floor((Math.random() * replies.length));
-        let question = arguments.slice(0).join(" ");
-
-        const embed = new Discord.MessageEmbed()
+        const embed = new MessageEmbed()
             .setAuthor(message.author.tag)
             .setColor("#FF9900")
             .addField("Question", question)
-            .addField("Answer", replies[result]);
+            .addField("Answer", replies[result])
 
-        message.channel.send(embed);
+        message.channel.send(embed)
     }
 }
